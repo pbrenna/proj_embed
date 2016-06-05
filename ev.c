fdf30995176465185201e24bb61789a878d80d6e
Pietro Brenna <pietrobrenna@hotmail.it>
2016-06-05 12:23:46 +0200

    Disable events when firing

--- ev.c
+++ ev.c
@@ -5,9 +5,11 @@ unsigned long ev_flags = 0;
 char ciao[] = "@ciao";
 void dispatch(){
 	IF_EV(ev_i2c_interrupt) {
+		EV_DISABLE(ev_i2c_interrupt);
 		i2c_state_machine();
 	}
 	IF_EV(ev_ciao){
+		EV_DISABLE(ev_ciao);
 		display_write(ciao, 5, ev_nop);
 	}
 }
