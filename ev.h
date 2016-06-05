b3d107a1639b8b142622fe9caf703db15a4ce325
Pietro Brenna <pietrobrenna@hotmail.it>
2016-06-05 12:22:03 +0200

    Disable events when firing

--- ev.h
+++ ev.h
@@ -1,4 +1,5 @@
-#define EV_ENABLE(ev_enum) ev_flags |= (1 << ev_enum)
+#define EV_ENABLE(ev_enum) (ev_flags |= (1 << ev_enum))
+#define EV_DISABLE(ev_enum) (ev_flags &= ~(1 << ev_enum))
 #define IF_EV(ev_num) if(ev_flags & (1 << ev_num))
 
 extern unsigned long ev_flags;
