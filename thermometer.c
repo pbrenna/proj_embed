142912a7c3d2c7e089fe1b64cb20940672253c45
AlessandroBregoli <alessandroxciv@gmail.com>
2016-06-06 16:50:01 +0200

    Add union int_chars used in thermometer. Modified thermometer_read and thermometer_save; now use int_char

--- thermometer.c
+++ thermometer.c
@@ -3,18 +3,22 @@
 #include "thermometer.h"
 #include "util.h"
 
-unsigned char therm_data[2]={0,0};
-unsigned char therm_data_old[2]={0,0};
+
+int therm_old = 0;
 
 
-
+int_chars therm_data;
 code char magia[] = {0x08, 0x40, 0x50, 0x50};
 void thermometer_read(){
-	i2c_command(TERM,magia,4, ev_thermometer_save, I2C_STOP, 2, therm_data);
+	i2c_command(TERM,magia,4, ev_thermometer_save, I2C_STOP, 2, therm_data.raw);
 }
 char disp_temp[] = "@        ";
 void thermometer_save(){
-	num2string(therm_data[1], disp_temp+2);
-	num2string(therm_data[1], disp_temp+6);
-	display_write(disp_temp, 9, ev_thermometer_read);
+
+	if(therm_data.raw[1] & 0x10)
+		therm_data.raw[1] |= 0xF0;
+	therm_data.temp *= 0.0625;
+	num2string(therm_data.temp, disp_temp+1);
+	display_write(disp_temp,5,ev_nop);
 }
+
