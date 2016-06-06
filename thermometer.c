dc4c229bf415dcc0c98f31aa1d712f98f5e79cb6
Pietro Brenna <pietrobrenna@hotmail.it>
2016-06-06 19:29:22 +0200

    Fixed temperature display and calculation; negative temperature still not working

--- thermometer.c
+++ thermometer.c
@@ -14,11 +14,15 @@ void thermometer_read(){
 }
 char disp_temp[] = "@        ";
 void thermometer_save(){
-
-	if(therm_data.raw[1] & 0x10)
-		therm_data.raw[1] |= 0xF0;
-	therm_data.temp *= 0.0625;
-	num2string(therm_data.temp, disp_temp+1);
-	display_write(disp_temp,5,ev_nop);
+	unsigned char len;
+	if(therm_data.raw[0] & 0x10)
+		therm_data.raw[0] |= 0xF0;
+	therm_data.temp = therm_data.temp / 16;
+	len = num2string(therm_data.temp/10, disp_temp+2);
+	num2string(therm_data.temp%10, disp_temp + 2+len);
+	disp_temp[2+len]='.';
+	disp_temp[4+len]='`';
+	disp_temp[5+len]='C';
+	display_write(disp_temp,9,ev_nop);
 }
 
